89aec72c7371baaa3e4063c2b8f7e50b1c24704c
Andres Noboa <andresnoboa17@hotmail.com>
2021-12-11 18:04:52 -0500

    Created an isAuth middleware

---
+++ middlewares/isAuth.js
@@ -0,0 +1,33 @@
+'use strict'
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const authHeader = req.get('Authorization');
+    if (!authHeader) {
+        const error = new Error("Not authenticated");
+        error.statusCode = 401;
+        throw error;
+    }
+
+    const SECRET = process.env.SECRET;
+
+    const token = authHeader.split(' ')[1];
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, SECRET);
+    } catch (err) {
+        if (!error.statusCode) {
+            error.statusCode = 500;
+            throw error;
+        }
+    }
+
+    if (!decodedToken) {
+        const error = new Error('Not authenticated.');
+        error.statusCode = 401;
+        throw error;
+    }
+
+    req.userId = decodedToken.userId;
+    next();
+}
